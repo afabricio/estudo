b5018b66b0a08e209e64e157c0c7d18825254604
afabricio <andrefabriciodossantos@gmail.com>
2018-08-13 16:58:37 -0300

    ref

---
+++ eureka-feign-client/src/main/java/br/com/study/microservice/FeignClientApplication.java
@@ -0,0 +1,35 @@
+package br.com.study.microservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@RestController
+public class FeignClientApplication 
+{
+	
+    @Autowired
+    private GreetingClient greetingClient;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientApplication.class, args);
+	}
+	
+	@RequestMapping("/get-greeting")
+    public String greeting() {
+
+        return greetingClient.greeting() ;
+    }
+	
+	
+
+}
---
+++ eureka-feign-client/src/main/java/br/com/study/microservice/GreetingClient.java
@@ -0,0 +1,12 @@
+package br.com.study.microservice;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("spring-cloud-eureka-client")
+public interface GreetingClient {
+
+	@RequestMapping("/greeting")
+	String  greeting();
+
+}
